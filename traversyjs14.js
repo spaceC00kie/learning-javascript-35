c6e2a5f0ddfe6b44767fd864153a9ffe5a3bd65f
spaceC00kie <kirstie317@gmail.com>
2020-12-29 20:34:39 -0500

    conditionals AND and OR and ternary operators ? and :

---
+++ traversyjs14.js
@@ -0,0 +1,32 @@
+//  conditionals
+
+//const x = 6
+//const y = 11
+
+// OR
+//if (x > 5 || y > 10) {
+//  console.log("x is more than 5 or y is more than 10")
+//}
+
+// AND
+//if (x > 5 && y > 10) {
+//    console.log("x is more than 5 and y is more than 10")
+//}
+
+// Nest if statements instead of and is too messy
+//if(x > 5) {
+//  if(y > 10) {
+//
+//  }
+//}
+
+
+
+// terinary operator
+
+const x = 10
+
+// ? means "then"   : means "else"
+const color = x > 10 ? "red" : "blue"
+
+console.log(color)
